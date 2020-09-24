bf0786368ec4ce33f6714e64c45f6e1f49d41a90
raketa92 <nazar.erdem@gmail.com>
2020-09-24 11:41:11 +0500

    edit TM index

--- index_tm.php
+++ index_tm.php
@@ -855,7 +855,7 @@
               <h4 class="underline heading-description">DESGALARYŇ GURLUŞYGY</h4>
 
               <p class="center description">
-              LEBAP WELAÝATYNYŇ KÖÝTENDAG ETRABYNDA  ÝYLLYK KUWWATLYLYGY 1 million TONNA SEMENT ÖNDÜRÝÄN ZAWODYNYN 2-NJI TAPGYRNY GURMAK WE TASLAMASYNY DÜZMEK.
+              LEBAP WELAÝATYNYŇ KÖÝTENDAG ETRABYNDA  ÝYLLYK KUWWATLYLYGY 1 MILLION TONNA SEMENT ÖNDÜRÝÄN ZAWODYNYN 2-NJI TAPGYRNY GURMAK WE TASLAMASYNY DÜZMEK.
               </p>
 
             </span>
